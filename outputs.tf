e53697a1f812a28aa7463f0b0c32d64305bf751d
Rob Levi <robert.levi@digital.justice.gov.uk>
2022-08-17 16:25:11 +0100

    feat: adds role ARNs to output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-# output "example_output" {
-#   description = "example output"
-#   value       = 
-# }
+output "app_account_role_arn" {
+    description = "The ARN of the role for managing application account resources"
+    value = module.app_account_role.iam_role_arn
+}
+
+output "control_panel_role_arn" {
+    description = "The ARN of the role that identifies the control panel"
+    value = module.control_panel_role.iam_role_arn
+}
+
+output "data_account_role_arn" {
+    description = "The ARN of the role for managing data account resources"
+    value = module.data_account_role.iam_role_arn
+}
